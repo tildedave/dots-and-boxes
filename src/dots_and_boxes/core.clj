4e768f6826fcc9a85f5c1999275033be3ff18178
Dave King <tildedave@gmail.com>
2018-02-12 17:20:25 -0800

    Horizontal/vertical partners

--- src/dots_and_boxes/core.clj
+++ src/dots_and_boxes/core.clj
@@ -15,8 +15,8 @@
 ; XXXXXXXXX  63 64 65 66 67 68 69 70 71
 ; XXXXXXXXX  72 73 74 75 76 77 78 79 80
 ;
-; 16's "pairs" in box life are 22, 24, 30
-; 24 is in two boxes: [16 22 24 30] and [18 24 26 32]
+; 21's "pairs" in box life are 21, 29, 31, 39
+; 31 is in two boxes: [31 21 29 39] and [31 23 33 41]
 
 ; FIXME: board-array is not yet an array
 (defrecord Board [height width board-array line-map])
@@ -77,8 +77,31 @@
         (< (mod idx (* 2 len)) len)))
 
 ; Board partners:
-; * horizontal items go up 2 rows and down 2 rows, then down 1 row
-; * vertical items go up/down 1 row and left/right,
+; * horizontal indices go up 2 rows and down 2 rows, then down 1 row
+; * vertical indices go up/down 1 row and left/right,
+
+(defn is-legit-box [board box]
+    ; This test relies on the 'X' padding in the board representation
+    (every? (fn [idx] (= (nth (:board-array board) idx) "-")) box))
+
+(defn horizontal-box-partners [board idx]
+    ; Returns a sequence of sets of box partners for a given index
+    (let [len (length-row (:width board))
+          up-box (set [idx (+ idx (- len) (- len)) (+ idx (- len) 1) (+ idx (- len) -1)])
+          down-box (set [idx (+ idx len len) (+ idx len 1) (+ idx len -1)])]
+          (filter (partial is-legit-box board) [up-box down-box])))
+
+(defn vertical-box-partners [board idx]
+    ; Returns a sequence of sets of box partners for a given index
+    (let [len (length-row (:width board))
+          left-box (set [idx (+ idx (- len) -1) (+ idx -2) (+ idx len -1)])
+          right-box (set [idx (+ idx (- len) 1) (+ idx +2) (+ idx len 1)])]
+          (filter (partial is-legit-box board) [left-box right-box])))
+
+(defn boxes-for-idx [board idx]
+    (if (is-horizontal-idx board idx)
+        (horizontal-box-partners board idx)
+        (vertical-box-partners board idx)))
 
 ; (defn vertical-partners-idx [board idx]
 ;     (let [len (length-row (:width board))]
--- test/dots_and_boxes/core_test.clj
+++ test/dots_and_boxes/core_test.clj
@@ -6,3 +6,9 @@
     (testing "Testing create-board functionality"
         (is (= (count (:board-array (create-board 3 3))) 81))
         (is (= (board-to-string (create-board 3 3)) "\n\n\n*-*-*\n- - -\n*-*-*\n- - -\n*-*-*\n\n"))))
+
+(deftest test-boxes-for-idx
+    ; Need to figure out how to really compare these
+    (testing "Testing boxes for index"
+        (is (= (seq (boxes-for-idx (create-board 3 3) 21)) (seq (set [21 39 31 29]))))
+        (is (= (seq (boxes-for-idx (create-board 3 3) 31)) (seq [(set [31 23 33 41]) (set [31 21 29 39])])))))
