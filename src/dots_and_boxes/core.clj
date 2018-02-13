dbb45f6c48073eb85a4fd988b4be9c4cd51bf2f3
Dave King <tildedave@gmail.com>
2018-02-12 16:56:51 -0800

    Add extra padding on left/right

--- src/dots_and_boxes/core.clj
+++ src/dots_and_boxes/core.clj
@@ -3,16 +3,17 @@
 
 ; Dots and boxes is played on N x N grid
 ; We represent 'out of bounds' with dummy rows that contain "X" items
-; For example a 3x3 grid will be internally represented as:
-; XXXXXXX   0  1  2  3  4  5  6
-; XXXXXXX   7  8  9 10 11 12 13
-; X*-*-*X  14 15 16 17 18 19 20
-; X- - -X  21 22 23 24 25 26 27
-; X*-*-*X  28 29 30 31 32 33 34
-; X- - -X  35 36 37 38 39 40 41
-; X*-*-*X  42 43 44 45 46 47 48
-; XXXXXXX  49 50 51 52 53 54 55
-; XXXXXXX  56 57 58 59 60 61 62
+;
+; A 3x3 grid will be internally represented as:
+; XXXXXXXXX   0  1  2  3  4  5  6  7  8
+; XXXXXXXXX   9 10 11 12 13 14 15 16 17
+; XX*-*-*XX  18 19 20 21 22 23 24 25 26
+; XX- - -XX  27 28 29 30 31 32 33 34 35
+; XX*-*-*XX  36 37 38 39 40 41 42 43 44
+; XX- - -XX  45 46 47 48 49 50 51 52 53
+; XX*-*-*XX  54 55 56 57 58 59 60 61 62
+; XXXXXXXXX  63 64 65 66 67 68 69 70 71
+; XXXXXXXXX  72 73 74 75 76 77 78 79 80
 ;
 ; 16's "pairs" in box life are 22, 24, 30
 ; 24 is in two boxes: [16 22 24 30] and [18 24 26 32]
@@ -20,7 +21,7 @@
 ; FIXME: board-array is not yet an array
 (defrecord Board [height width board-array line-map])
 
-(defn length-row [width] (+ (* 2 width) 1))
+(defn length-row [width] (+ (* 2 width) 3))
 
 (defn create-dummy-row [width]
     (repeat (length-row width) "X"))
@@ -29,20 +30,20 @@
     (let [len (length-row width)]
         (map
             #(cond
-            (zero? %1) "X"
-            (= %1 (- len 1)) "X"
-            (zero? (mod %1 2)) "-"
-            :else "*")
+            (<= %1 1) "X"
+            (>= %1 (- len 2)) "X"
+            (zero? (mod %1 2)) "*"
+            :else "-")
             (range len))))
 
 (defn create-vertical-row [width]
     (let [len (length-row width)]
         (map
             #(cond
-            (zero? %1) "X"
-            (= %1 (- len 1)) "X"
-            (zero? (mod %1 2)) " "
-            :else "-")
+            (<= %1 1) "X"
+            (>= %1 (- len 2)) "X"
+            (zero? (mod %1 2)) "-"
+            :else " ")
             (range len))))
 
 (defn create-board [height width]
@@ -74,6 +75,10 @@
     ; Is the index on the horizontal row?
     (let [len (length-row (:width board))]
         (< (mod idx (* 2 len)) len)))
+
+; Board partners:
+; * horizontal items go up 2 rows and down 2 rows, then down 1 row
+; * vertical items go up/down 1 row and left/right,
 
 ; (defn vertical-partners-idx [board idx]
 ;     (let [len (length-row (:width board))]
--- test/dots_and_boxes/core_test.clj
+++ test/dots_and_boxes/core_test.clj
@@ -4,5 +4,5 @@
 
 (deftest test-create-board
     (testing "Testing create-board functionality"
-        (is (= (count (:board-array (create-board 3 3))) 63))
+        (is (= (count (:board-array (create-board 3 3))) 81))
         (is (= (board-to-string (create-board 3 3)) "\n\n\n*-*-*\n- - -\n*-*-*\n- - -\n*-*-*\n\n"))))
